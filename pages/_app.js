3382a35a40cf4b1027320126e64a04dff95997cd
kimminkyeu <minkyeki@student.42seoul.kr>
2022-11-02 14:18:07 +0900

    deleted dark_mode

--- pages/_app.js
+++ pages/_app.js
@@ -4,9 +4,7 @@ import { ThemeProvider } from 'next-themes'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider attribute='class'>
       <Component {...pageProps} />
-    </ThemeProvider>
   );
 }
 
