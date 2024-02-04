0a12ec384b12f5eca42435ea9948463b1897b944
kimminkyeu <minkyeki@student.42seoul.kr>
2024-02-04 17:57:42 +0900

    Fix: 블로그 이름 및 설명 변경

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,8 +1,8 @@
 import {ArticleStatus} from "@/app/api/type";
 
 export namespace Config {
-  export const BLOG_TITLE = 'Minky';
-  export const BLOG_DESCRIPTION = '민규 블로그';
+  export const BLOG_TITLE = 'Minky Blog';
+  export const BLOG_DESCRIPTION = '김민규 블로그';
 // for notion api Status
   export const STATUS_PUBLISHED_ARTICLE: ArticleStatus = 'Publish';
   export const STATUS_ABOUT_ME: ArticleStatus = 'About me';
